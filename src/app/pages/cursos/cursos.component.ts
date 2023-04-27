a112d1e77a2499eb383c5ce34417fe05702a7702
marisolkarina <marisol.kari.20@gmail.com>
2023-04-27 00:31:05 -0500

    cursos mockeados

--- src/app/pages/cursos/cursos.component.ts
+++ src/app/pages/cursos/cursos.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CursosService } from './services/cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./cursos.component.css']
 })
 export class CursosComponent {
-
+  constructor(
+    private cursosService: CursosService
+  ) {}
+  
+  ngOnInit(): void {
+    this.cursosService.obtenerCursos()
+      .subscribe(console.log)
+  }
 }
--- src/app/pages/cursos/services/cursos.service.ts
+++ src/app/pages/cursos/services/cursos.service.ts
@@ -2,6 +2,28 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { Curso } from '../models';
 
+const CURSOS_MOCKS: Curso[] = [
+  {
+    id: 1,
+    nombre: 'Geometría Analítica',
+    fecha_inicio: new Date(),
+    fecha_fin: new Date()
+  },
+  {
+    id: 2,
+    nombre: 'Cálculo Integral',
+    fecha_inicio: new Date(),
+    fecha_fin: new Date()
+  },
+  {
+    id: 3,
+    nombre: 'Estadística y Probabilidades',
+    fecha_inicio: new Date(),
+    fecha_fin: new Date()
+  }
+]
+
+
 @Injectable({
   providedIn: 'root'
 })
@@ -12,7 +34,8 @@ export class CursosService {
   
   constructor() { }
 
-  // obtenerCursos(): Observable<Curso[]> {
-
-  // }
+  obtenerCursos(): Observable<Curso[]> {
+    this.cursos$.next(CURSOS_MOCKS);
+    return this.cursos$.asObservable();
+  }
 }
